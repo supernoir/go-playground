b928c7f9d593cd0454db53a07c24b7a47225b5f8
supernoir <kawohl.bentodesign@gmail.com>
2017-08-09 17:09:17 +0200

    adds io

---
+++ io.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Create("sample.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file.WriteString("Hello World!")
+	file.Close()
+
+	stream, err := ioutil.ReadFile("sample.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	readString := string(stream)
+	fmt.Println(readString)
+
+}
